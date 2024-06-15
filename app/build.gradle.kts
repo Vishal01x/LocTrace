056b2efcfc10595396f82d186144e57c3219f2f7
KanhaiyaKumarShukla <kanhaiyashukla0.01@gmail.com>
2024-06-15 12:04:28 +0530

    third

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -41,7 +45,19 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // GeoFire
+    implementation ("com.firebase:geofire-android:3.1.0")
+
+    // Location services
+    implementation ("com.google.android.gms:play-services-location:21.3.0")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 }
--- app/src/main/java/com/exa/android/loctrace/MainActivity.kt
+++ app/src/main/java/com/exa/android/loctrace/MainActivity.kt
@@ -2,10 +2,30 @@ package com.exa.android.loctrace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.exa.android.loctrace.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding:ActivityMainBinding ?=null
+    private val binding get() = _binding!!
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHostFragment=supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+
+        navController=navHostFragment.navController
+        //navController.navigate(R.id.usersEntryFragment)
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
